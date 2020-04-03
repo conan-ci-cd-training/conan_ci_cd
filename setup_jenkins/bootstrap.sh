69c81cf24788665cbb60b1a0c959d98cd82b8d76
czoido <mrgalleta@gmail.com>
2020-04-03 10:29:43 +0200

    add bootstrap

---
+++ setup_jenkins/bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# bootstrap jenkins
+
+# param 1: Orbitera IP
+# param 2: Artifactory password
+# param 3: Jenkins administrator password
+
+docker exec -it jenkins /bin/bash -c "curl https://raw.githubusercontent.com/conan-ci-cd-training/conan_ci_cd/master/setup_jenkins/init_jenkins.sh -O;./init_jenkins.sh $1 $2 $3"
