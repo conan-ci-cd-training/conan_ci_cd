28590ef9c05c0168e66d0934c0e0bb46dd008f93
czoido <mrgalleta@gmail.com>
2020-04-03 10:23:22 +0200

    add jenkins jobs

--- setup_jenkins/init_jenkins.sh
+++ setup_jenkins/init_jenkins.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # bootstrap jenkins
-# param 1: Artifactory IP
+
+# param 1: Orbitera IP
 # param 2: Artifactory password
+# param 3: Jenkins administrator password
 
 echo "Clone libraries"
+
+cd /var/lib/jenkins
 
 git clone https://github.com/conan-ci-cd-training/libA.git
 git clone https://github.com/conan-ci-cd-training/libB.git
@@ -29,3 +33,9 @@ conan upload '*' -r conan-develop --confirm  --force
 
 docker run -it conanio/gcc8 /bin/bash  -c "conan config install https://github.com/conan-ci-cd-training/settings.git;conan remote add conan-develop http://$1:8081/artifactory/api/conan/conan-develop;conan remote add conan-tmp http://$1:8081/artifactory/api/conan/conan-tmp;conan user -p $2 -r conan-develop admin;conan user -p $2 -r conan-tmp admin;conan install App/1.0@mycompany/stable --profile conanio-gcc8 --build missing -r conan-tmp;conan install App2/1.0@mycompany/stable --profile conanio-gcc8 --build missing -r conan-tmp;conan upload '*' --all -r conan-tmp --confirm  --force;conan upload '*' --all -r conan-develop --confirm  --force"
 docker run -it conanio/gcc7 /bin/bash  -c "conan config install https://github.com/conan-ci-cd-training/settings.git;conan remote add conan-develop http://$1:8081/artifactory/api/conan/conan-develop;conan remote add conan-tmp http://$1:8081/artifactory/api/conan/conan-tmp;conan user -p $2 -r conan-develop admin;conan user -p $2 -r conan-tmp admin;conan install App/1.0@mycompany/stable --profile conanio-gcc7 --build missing -r conan-tmp;conan install App2/1.0@mycompany/stable --profile conanio-gcc7 --build missing -r conan-tmp;conan upload '*' --all -r conan-tmp --confirm  --force;conan upload '*' --all -r conan-develop --confirm  --force"
+
+curl https://raw.githubusercontent.com/conan-ci-cd-training/conan_ci_cd/master/setup_jenkins/jenkins_jobs.tgz -O
+
+tar -xvf jenkins_jobs.tgz --directory /var/lib/jenkins/jobs
+
+curl -X POST -u administrator:$3 http://$1:8080/restart
