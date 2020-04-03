21b1372c67aa85fa65b2091e52d79e3eb03b2694
czoido <mrgalleta@gmail.com>
2020-04-03 10:37:28 +0200

    add permissions

--- setup_jenkins/bootstrap.sh
+++ setup_jenkins/bootstrap.sh
@@ -6,4 +6,4 @@
 # param 2: Artifactory password
 # param 3: Jenkins administrator password
 
-docker exec -it jenkins /bin/bash -c "curl https://raw.githubusercontent.com/conan-ci-cd-training/conan_ci_cd/master/setup_jenkins/init_jenkins.sh -O;./init_jenkins.sh $1 $2 $3"
+docker exec -it jenkins /bin/bash -c "curl https://raw.githubusercontent.com/conan-ci-cd-training/conan_ci_cd/master/setup_jenkins/init_jenkins.sh -O;chmod +x init_jenkins.sh;./init_jenkins.sh $1 $2 $3"
