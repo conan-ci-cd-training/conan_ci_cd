59cd904d0696871e082557717648174c33612646
czoido <mrgalleta@gmail.com>
2020-04-08 14:27:00 +0200

    wip

--- setup_jenkins/init_jenkins.sh
+++ setup_jenkins/init_jenkins.sh
@@ -13,6 +13,8 @@ cd /var/lib/jenkins
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 
+conan config install https://github.com/conan-ci-cd-training/settings.git
+
 git clone https://github.com/conan-ci-cd-training/libA.git
 git clone https://github.com/conan-ci-cd-training/libB.git
 git clone https://github.com/conan-ci-cd-training/libC.git
