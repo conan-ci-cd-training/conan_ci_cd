d1aa150ed25ddc5b91b892956034cd5a8f602b9e
czoido <mrgalleta@gmail.com>
2020-04-08 08:23:52 +0200

    upload to develop

--- setup_jenkins/init_jenkins.sh
+++ setup_jenkins/init_jenkins.sh
@@ -31,11 +31,10 @@ conan remote add conan-tmp http://$1:8081/artifactory/api/conan/conan-tmp
 conan user -p $2 -r conan-develop admin
 conan user -p $2 -r conan-tmp admin
 
-conan upload '*' -r conan-tmp --confirm  --force
 conan upload '*' -r conan-develop --confirm  --force
 
-docker run -it conanio/gcc8 /bin/bash  -c "conan config install https://github.com/conan-ci-cd-training/settings.git;conan remote add conan-develop http://$1:8081/artifactory/api/conan/conan-develop;conan remote add conan-tmp http://$1:8081/artifactory/api/conan/conan-tmp;conan user -p $2 -r conan-develop admin;conan user -p $2 -r conan-tmp admin;conan install App/1.0@mycompany/stable --profile conanio-gcc8 --build missing -r conan-tmp;conan install App2/1.0@mycompany/stable --profile conanio-gcc8 --build missing -r conan-tmp;conan upload '*' --all -r conan-tmp --confirm  --force;conan upload '*' --all -r conan-develop --confirm  --force"
-docker run -it conanio/gcc7 /bin/bash  -c "conan config install https://github.com/conan-ci-cd-training/settings.git;conan remote add conan-develop http://$1:8081/artifactory/api/conan/conan-develop;conan remote add conan-tmp http://$1:8081/artifactory/api/conan/conan-tmp;conan user -p $2 -r conan-develop admin;conan user -p $2 -r conan-tmp admin;conan install App/1.0@mycompany/stable --profile conanio-gcc7 --build missing -r conan-tmp;conan install App2/1.0@mycompany/stable --profile conanio-gcc7 --build missing -r conan-tmp;conan upload '*' --all -r conan-tmp --confirm  --force;conan upload '*' --all -r conan-develop --confirm  --force"
+docker run -it conanio/gcc8 /bin/bash  -c "conan config install https://github.com/conan-ci-cd-training/settings.git;conan remote add conan-develop http://$1:8081/artifactory/api/conan/conan-develop;conan user -p $2 -r conan-develop admin;conan install App/1.0@mycompany/stable --profile conanio-gcc8 --build missing -r conan-develop;conan install App2/1.0@mycompany/stable --profile conanio-gcc8 --build missing -r conan-develop;conan upload '*' --all -r conan-develop --confirm  --force"
+docker run -it conanio/gcc7 /bin/bash  -c "conan config install https://github.com/conan-ci-cd-training/settings.git;conan remote add conan-develop http://$1:8081/artifactory/api/conan/conan-develop;conan user -p $2 -r conan-develop admin;conan install App/1.0@mycompany/stable --profile conanio-gcc7 --build missing -r conan-develop;conan install App2/1.0@mycompany/stable --profile conanio-gcc7 --build missing -r conan-develop;conan upload '*' --all -r conan-develop --confirm  --force"
 
 curl https://raw.githubusercontent.com/conan-ci-cd-training/conan_ci_cd/master/setup_jenkins/jenkins_jobs.tgz -O
 
