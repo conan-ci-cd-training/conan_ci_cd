17ae7ab093ded2cac6ffafcaaa51df80ad9c0692
czoido <mrgalleta@gmail.com>
2020-04-08 16:33:18 +0200

    add exercise

---
+++ phase1/phase1.sh
@@ -0,0 +1,15 @@
+
+# Exercise
+
+git clone https://github.com/conan-ci-cd-training/libA.git
+cd libA
+git checkout feature/add_comments
+
+# want to build with several profiles
+# debug
+conan graph lock . --lockfile=../lockfiles/debug.lock -r conan-develop --profile debug-gcc6  
+conan create . mycompany/stable --lockfile=../lockfiles/debug.lock -r conan-develop --profile debug-gcc6 
+
+#release
+conan graph lock . --lockfile=../lockfiles/release.lock -r conan-develop --profile release-gcc6  
+conan create . mycompany/stable --lockfile=../lockfiles/release.lock -r conan-develop --profile release-gcc6 
