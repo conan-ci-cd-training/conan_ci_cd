7b38a69f06aa8a6693b0db140e1a214813398243
czoido <mrgalleta@gmail.com>
2020-04-09 12:42:52 +0200

    update jobs with libD

--- setup_jenkins/init_jenkins.sh
+++ setup_jenkins/init_jenkins.sh
@@ -40,8 +40,8 @@ conan upload '*' -r conan-develop --confirm  --force
 docker run -it conanio/gcc8 /bin/bash  -c "conan config install https://github.com/conan-ci-cd-training/settings.git;conan remote add conan-develop http://$1:8081/artifactory/api/conan/conan-develop;conan user -p $2 -r conan-develop admin;conan install App/1.0@mycompany/stable --profile conanio-gcc8 --build missing -r conan-develop;conan install App2/1.0@mycompany/stable --profile conanio-gcc8 --build missing -r conan-develop;conan upload '*' --all -r conan-develop --confirm  --force"
 docker run -it conanio/gcc7 /bin/bash  -c "conan config install https://github.com/conan-ci-cd-training/settings.git;conan remote add conan-develop http://$1:8081/artifactory/api/conan/conan-develop;conan user -p $2 -r conan-develop admin;conan install App/1.0@mycompany/stable --profile conanio-gcc7 --build missing -r conan-develop;conan install App2/1.0@mycompany/stable --profile conanio-gcc7 --build missing -r conan-develop;conan upload '*' --all -r conan-develop --confirm  --force"
 
-#curl https://raw.githubusercontent.com/conan-ci-cd-training/conan_ci_cd/master/setup_jenkins/jenkins_jobs.tgz -O
+curl https://raw.githubusercontent.com/conan-ci-cd-training/conan_ci_cd/master/setup_jenkins/jenkins_jobs.tgz -O
 
-#tar -xvf jenkins_jobs.tgz --directory /var/lib/jenkins/jobs
+tar -xvf jenkins_jobs.tgz --directory /var/lib/jenkins/jobs
 
-#curl -X POST -u administrator:$3 http://$1:8080/restart
+curl -X POST -u administrator:$3 http://$1:8080/restart
