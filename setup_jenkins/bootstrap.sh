694985e6fdf419673de834822191d3d7a1954cc1
czoido <mrgalleta@gmail.com>
2020-04-10 09:32:28 +0200

    use jfrog.local as address

--- setup_jenkins/bootstrap.sh
+++ setup_jenkins/bootstrap.sh
@@ -2,41 +2,43 @@
 
 # bootstrap jenkins
 
-# param 1: Orbitera IP
-# param 2: Artifactory password
-# param 3: Jenkins administrator password
+# param 1: Artifactory password
+# param 2: Jenkins administrator password
 
 permission=conan-ci2
 user=conan
 password=conan2020
+address=jfrog.local
+artifactory_pass=$1
+jenkins_pass=$2
 
 ## Artifactory configuration
 
-curl -uadmin:$2 -XPOST http://$1/artifactory/api/security/groups/readers -d '{"autoJoin":"false"}' -H "Content-Type: application/json"
+curl -uadmin:${artifactory_pass} -XPOST http://${address}/artifactory/api/security/groups/readers -d '{"autoJoin":"false"}' -H "Content-Type: application/json"
 
 # create repo
 sed "s/<REPO_NAME>/conan-tmp/" templates/create_repo.json | sed "s/<REPO_TYPE>/conan/" | sed "s/<REPO_LAYOUT>/conan-default/" > conan-tmp.json
 sed "s/<REPO_NAME>/conan-develop/" templates/create_repo.json | sed "s/<REPO_TYPE>/conan/" | sed "s/<REPO_LAYOUT>/conan-default/" > conan-develop.json
 sed "s/<REPO_NAME>/conan-metadata/" templates/create_repo.json | sed "s/<REPO_TYPE>/generic/" | sed "s/<REPO_LAYOUT>/simple-default/" > conan-metadata.json
 
-curl -uadmin:$2 -XPUT http://$1/artifactory/api/repositories/conan-tmp -T conan-tmp.json -H "Content-Type: application/json"
-curl -uadmin:$2 -XPUT http://$1/artifactory/api/repositories/conan-develop -T conan-develop.json -H "Content-Type: application/json"
-curl -uadmin:$2 -XPUT http://$1/artifactory/api/repositories/conan-metadata -T conan-metadata.json -H "Content-Type: application/json"
+curl -uadmin:${artifactory_pass} -XPUT http://${address}/artifactory/api/repositories/conan-tmp -T conan-tmp.json -H "Content-Type: application/json"
+curl -uadmin:${artifactory_pass} -XPUT http://${address}/artifactory/api/repositories/conan-develop -T conan-develop.json -H "Content-Type: application/json"
+curl -uadmin:${artifactory_pass} -XPUT http://${address}/artifactory/api/repositories/conan-metadata -T conan-metadata.json -H "Content-Type: application/json"
 
 # create user
 sed "s/<USER>/${user}/" templates/create_user.json | sed "s/<PASSWORD>/${password}/" > user.json
-curl -uadmin:$2 -XPUT http://$1/artifactory/api/security/users/${user} -T user.json -H "Content-Type: application/json"
+curl -uadmin:${artifactory_pass} -XPUT http://${address}/artifactory/api/security/users/${user} -T user.json -H "Content-Type: application/json"
 
 # create permission
 sed "s/<USER>/${user}/" templates/create_permission.json | sed "s/<NAME>/${permission}/"  | sed "s/<REPO1>/conan-tmp/"| sed "s/<REPO2>/conan-develop/" | sed "s/<REPO3>/conan-metadata/" > permission.json
-curl -uadmin:$2 -XPUT http://$1/artifactory/api/v2/security/permissions/${permission} -T permission.json -H "Content-Type: application/json"
+curl -uadmin:${artifactory_pass} -XPUT http://${address}/artifactory/api/v2/security/permissions/${permission} -T permission.json -H "Content-Type: application/json"
 
 ## Conan client configuration
 
 conan config install https://github.com/conan-ci-cd-training/settings.git
 
-conan remote add conan-develop http://$1:8081/artifactory/api/conan/conan-develop
-conan remote add conan-tmp http://$1:8081/artifactory/api/conan/conan-tmp
+conan remote add conan-develop http://${address}:8081/artifactory/api/conan/conan-develop
+conan remote add conan-tmp http://${address}:8081/artifactory/api/conan/conan-tmp
 conan user -p ${password} -r conan-develop ${user}
 conan user -p ${password} -r conan-tmp ${user}
 
@@ -45,4 +47,4 @@ git config --global user.name "Your Name"
 
 ## Jenkins configuration
 
-docker exec -it jenkins /bin/bash -c "curl https://raw.githubusercontent.com/conan-ci-cd-training/conan_ci_cd/master/setup_jenkins/init_jenkins.sh -O;chmod +x init_jenkins.sh;./init_jenkins.sh $1 $2 $3"
+docker exec -it jenkins /bin/bash -c "curl https://raw.githubusercontent.com/conan-ci-cd-training/conan_ci_cd/master/setup_jenkins/init_jenkins.sh -O;chmod +x init_jenkins.sh;./init_jenkins.sh ${artifactory_pass} ${jenkins_pass}"
--- setup_jenkins/init_jenkins.sh
+++ setup_jenkins/init_jenkins.sh
@@ -2,9 +2,12 @@
 
 # bootstrap jenkins
 
-# param 1: Orbitera IP
-# param 2: Artifactory password
-# param 3: Jenkins administrator password
+# param 1: Artifactory password
+# param 2: Jenkins administrator password
+
+address=jfrog.local
+artifactory_pass=$1
+jenkins_pass=$2
 
 echo "Clone libraries"
 
@@ -30,18 +33,18 @@ conan export libD mycompany/stable
 conan export App mycompany/stable
 conan export App2 mycompany/stable
 
-conan remote add conan-develop http://$1:8081/artifactory/api/conan/conan-develop
-conan remote add conan-tmp http://$1:8081/artifactory/api/conan/conan-tmp
-conan user -p $2 -r conan-develop admin
-conan user -p $2 -r conan-tmp admin
+conan remote add conan-develop http://${address}:8081/artifactory/api/conan/conan-develop
+conan remote add conan-tmp http://${address}:8081/artifactory/api/conan/conan-tmp
+conan user -p ${artifactory_pass} -r conan-develop admin
+conan user -p ${artifactory_pass} -r conan-tmp admin
 
 conan upload '*' -r conan-develop --confirm  --force
 
-docker run -it conanio/gcc8 /bin/bash  -c "conan config install https://github.com/conan-ci-cd-training/settings.git;conan remote add conan-develop http://$1:8081/artifactory/api/conan/conan-develop;conan user -p $2 -r conan-develop admin;conan install App/1.0@mycompany/stable --profile conanio-gcc8 --build missing -r conan-develop;conan install App2/1.0@mycompany/stable --profile conanio-gcc8 --build missing -r conan-develop;conan upload '*' --all -r conan-develop --confirm  --force"
-docker run -it conanio/gcc7 /bin/bash  -c "conan config install https://github.com/conan-ci-cd-training/settings.git;conan remote add conan-develop http://$1:8081/artifactory/api/conan/conan-develop;conan user -p $2 -r conan-develop admin;conan install App/1.0@mycompany/stable --profile conanio-gcc7 --build missing -r conan-develop;conan install App2/1.0@mycompany/stable --profile conanio-gcc7 --build missing -r conan-develop;conan upload '*' --all -r conan-develop --confirm  --force"
+docker run -it conanio/gcc8 /bin/bash  -c "conan config install https://github.com/conan-ci-cd-training/settings.git;conan remote add conan-develop http://${address}:8081/artifactory/api/conan/conan-develop;conan user -p ${artifactory_pass} -r conan-develop admin;conan install App/1.0@mycompany/stable --profile conanio-gcc8 --build missing -r conan-develop;conan install App2/1.0@mycompany/stable --profile conanio-gcc8 --build missing -r conan-develop;conan upload '*' --all -r conan-develop --confirm  --force"
+docker run -it conanio/gcc7 /bin/bash  -c "conan config install https://github.com/conan-ci-cd-training/settings.git;conan remote add conan-develop http://${address}:8081/artifactory/api/conan/conan-develop;conan user -p ${artifactory_pass} -r conan-develop admin;conan install App/1.0@mycompany/stable --profile conanio-gcc7 --build missing -r conan-develop;conan install App2/1.0@mycompany/stable --profile conanio-gcc7 --build missing -r conan-develop;conan upload '*' --all -r conan-develop --confirm  --force"
 
 curl https://raw.githubusercontent.com/conan-ci-cd-training/conan_ci_cd/master/setup_jenkins/jenkins_jobs.tgz -O
 
 tar -xvf jenkins_jobs.tgz --directory /var/lib/jenkins/jobs
 
-curl -X POST -u administrator:$3 http://$1:8080/restart
+curl -X POST -u administrator:${jenkins_pass} http://${address}:8080/restart
