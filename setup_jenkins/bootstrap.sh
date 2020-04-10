183d7cd5680712aba0d3792dbb2f55e4f9146177
czoido <mrgalleta@gmail.com>
2020-04-10 09:39:36 +0200

    minor changes

--- setup_jenkins/bootstrap.sh
+++ setup_jenkins/bootstrap.sh
@@ -7,16 +7,16 @@
 
 permission=conan-ci2
 user=conan
-password=conan2020
-address=jfrog.local
+password="conan2020"
+address="jfrog.local"
 artifactory_pass=$1
 jenkins_pass=$2
 
-## Artifactory configuration
+echo "------ Artifactory configuration ------"
 
 curl -uadmin:${artifactory_pass} -XPOST http://${address}/artifactory/api/security/groups/readers -d '{"autoJoin":"false"}' -H "Content-Type: application/json"
 
-# create repo
+echo "create repo"
 sed "s/<REPO_NAME>/conan-tmp/" templates/create_repo.json | sed "s/<REPO_TYPE>/conan/" | sed "s/<REPO_LAYOUT>/conan-default/" > conan-tmp.json
 sed "s/<REPO_NAME>/conan-develop/" templates/create_repo.json | sed "s/<REPO_TYPE>/conan/" | sed "s/<REPO_LAYOUT>/conan-default/" > conan-develop.json
 sed "s/<REPO_NAME>/conan-metadata/" templates/create_repo.json | sed "s/<REPO_TYPE>/generic/" | sed "s/<REPO_LAYOUT>/simple-default/" > conan-metadata.json
@@ -25,15 +25,15 @@ curl -uadmin:${artifactory_pass} -XPUT http://${address}/artifactory/api/reposit
 curl -uadmin:${artifactory_pass} -XPUT http://${address}/artifactory/api/repositories/conan-develop -T conan-develop.json -H "Content-Type: application/json"
 curl -uadmin:${artifactory_pass} -XPUT http://${address}/artifactory/api/repositories/conan-metadata -T conan-metadata.json -H "Content-Type: application/json"
 
-# create user
+echo "create user"
 sed "s/<USER>/${user}/" templates/create_user.json | sed "s/<PASSWORD>/${password}/" > user.json
 curl -uadmin:${artifactory_pass} -XPUT http://${address}/artifactory/api/security/users/${user} -T user.json -H "Content-Type: application/json"
 
-# create permission
+echo "create permission"
 sed "s/<USER>/${user}/" templates/create_permission.json | sed "s/<NAME>/${permission}/"  | sed "s/<REPO1>/conan-tmp/"| sed "s/<REPO2>/conan-develop/" | sed "s/<REPO3>/conan-metadata/" > permission.json
 curl -uadmin:${artifactory_pass} -XPUT http://${address}/artifactory/api/v2/security/permissions/${permission} -T permission.json -H "Content-Type: application/json"
 
-## Conan client configuration
+echo "------ Conan client configuration ------"
 
 conan config install https://github.com/conan-ci-cd-training/settings.git
 
@@ -45,6 +45,6 @@ conan user -p ${password} -r conan-tmp ${user}
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 
-## Jenkins configuration
+echo "------ Jenkins configuration ------"
 
 docker exec -it jenkins /bin/bash -c "curl https://raw.githubusercontent.com/conan-ci-cd-training/conan_ci_cd/master/setup_jenkins/init_jenkins.sh -O;chmod +x init_jenkins.sh;./init_jenkins.sh ${artifactory_pass} ${jenkins_pass}"
--- setup_jenkins/init_jenkins.sh
+++ setup_jenkins/init_jenkins.sh
@@ -5,7 +5,7 @@
 # param 1: Artifactory password
 # param 2: Jenkins administrator password
 
-address=jfrog.local
+address="jfrog.local"
 artifactory_pass=$1
 jenkins_pass=$2
 
@@ -43,8 +43,12 @@ conan upload '*' -r conan-develop --confirm  --force
 docker run -it conanio/gcc8 /bin/bash  -c "conan config install https://github.com/conan-ci-cd-training/settings.git;conan remote add conan-develop http://${address}:8081/artifactory/api/conan/conan-develop;conan user -p ${artifactory_pass} -r conan-develop admin;conan install App/1.0@mycompany/stable --profile conanio-gcc8 --build missing -r conan-develop;conan install App2/1.0@mycompany/stable --profile conanio-gcc8 --build missing -r conan-develop;conan upload '*' --all -r conan-develop --confirm  --force"
 docker run -it conanio/gcc7 /bin/bash  -c "conan config install https://github.com/conan-ci-cd-training/settings.git;conan remote add conan-develop http://${address}:8081/artifactory/api/conan/conan-develop;conan user -p ${artifactory_pass} -r conan-develop admin;conan install App/1.0@mycompany/stable --profile conanio-gcc7 --build missing -r conan-develop;conan install App2/1.0@mycompany/stable --profile conanio-gcc7 --build missing -r conan-develop;conan upload '*' --all -r conan-develop --confirm  --force"
 
+echo "------ Configure Jenkins jobs ------"
+
 curl https://raw.githubusercontent.com/conan-ci-cd-training/conan_ci_cd/master/setup_jenkins/jenkins_jobs.tgz -O
 
 tar -xvf jenkins_jobs.tgz --directory /var/lib/jenkins/jobs
+
+echo "------ Restart Jenkins ------"
 
 curl -X POST -u administrator:${jenkins_pass} http://${address}:8080/restart
