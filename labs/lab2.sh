9f07de92e409cf5dccbb42cf003da8c4be7cd272
czoido <mrgalleta@gmail.com>
2020-04-15 20:12:36 +0200

    fix

--- labs/lab2.sh
+++ labs/lab2.sh
@@ -2,15 +2,15 @@
 
 ## lab2: 
 
-$ git clone https://github.com/conan-ci-cd-training/libB.git
-$ cd libB
-$ git checkout feature/add_comments
+git clone https://github.com/conan-ci-cd-training/libB.git
+cd libB
+git checkout feature/add_comments
 # we want the library to be tested for different configurations: 
 
 # generate lockfiles for all configurations (debug and release)
-$ conan graph lock libB/1.0@mycompany/stable --lockfile=../lockfiles/debug.lock -r conan-develop --profile debug-gcc6 
-$ conan graph lock libB/1.0@mycompany/stable --lockfile=../lockfiles/release.lock -r conan-develop --profile release-gcc6
+conan graph lock libB/1.0@mycompany/stable --lockfile=../lockfiles/debug.lock -r conan-develop --profile debug-gcc6 
+conan graph lock libB/1.0@mycompany/stable --lockfile=../lockfiles/release.lock -r conan-develop --profile release-gcc6
 
 # create packages with those lockfiles
-$ conan create . mycompany/stable --lockfile=../lockfiles/debug.lock
-$ conan create . mycompany/stable --lockfile=../lockfiles/release.lock
+conan create . mycompany/stable --lockfile=../lockfiles/debug.lock
+conan create . mycompany/stable --lockfile=../lockfiles/release.lock
