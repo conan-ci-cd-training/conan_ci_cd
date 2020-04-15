207d98ef3f4cb2ba97d202f261c3a00c55caa625
Yann Chaysinh <yann.chaysinh@gmail.com>
2020-04-15 12:16:19 +0000

    added debian repo + update conan permission

--- setup_jenkins/bootstrap.sh
+++ setup_jenkins/bootstrap.sh
@@ -21,16 +21,22 @@ sed "s/<REPO_NAME>/conan-tmp/" templates/create_repo.json | sed "s/<REPO_TYPE>/c
 sed "s/<REPO_NAME>/conan-develop/" templates/create_repo.json | sed "s/<REPO_TYPE>/conan/" | sed "s/<REPO_LAYOUT>/conan-default/" > conan-develop.json
 sed "s/<REPO_NAME>/conan-metadata/" templates/create_repo.json | sed "s/<REPO_TYPE>/generic/" | sed "s/<REPO_LAYOUT>/simple-default/" > conan-metadata.json
 
+sed "s/<REPO_NAME>/app-debian-sit-local/" templates/create_repo.json | sed "s/<REPO_TYPE>/debian/" | sed "s/<REPO_LAYOUT>/simple-default/" > debian-sit.json
+sed "s/<REPO_NAME>/app-debian-uat-local/" templates/create_repo.json | sed "s/<REPO_TYPE>/debian/" | sed "s/<REPO_LAYOUT>/simple-default/" > debian-uat.json
+
 curl -uadmin:${artifactory_pass} -XPUT http://${address}/artifactory/api/repositories/conan-tmp -T conan-tmp.json -H "Content-Type: application/json"
 curl -uadmin:${artifactory_pass} -XPUT http://${address}/artifactory/api/repositories/conan-develop -T conan-develop.json -H "Content-Type: application/json"
 curl -uadmin:${artifactory_pass} -XPUT http://${address}/artifactory/api/repositories/conan-metadata -T conan-metadata.json -H "Content-Type: application/json"
+
+curl -uadmin:${artifactory_pass} -XPUT http://${address}/artifactory/api/repositories/app-debian-sit-local -T debian-sit.json -H "Content-Type: application/json"
+curl -uadmin:${artifactory_pass} -XPUT http://${address}/artifactory/api/repositories/app-debian-uat-local -T debian-uat.json -H "Content-Type: application/json"
 
 echo "create user"
 sed "s/<USER>/${user}/" templates/create_user.json | sed "s/<PASSWORD>/${password}/" > user.json
 curl -uadmin:${artifactory_pass} -XPUT http://${address}/artifactory/api/security/users/${user} -T user.json -H "Content-Type: application/json"
 
 echo "create permission"
-sed "s/<USER>/${user}/" templates/create_permission.json | sed "s/<NAME>/${permission}/"  | sed "s/<REPO1>/conan-tmp/"| sed "s/<REPO2>/conan-develop/" | sed "s/<REPO3>/conan-metadata/" > permission.json
+sed "s/<USER>/${user}/" templates/create_permission.json | sed "s/<NAME>/${permission}/"  | sed "s/<REPO1>/conan-tmp/"| sed "s/<REPO2>/conan-develop/" | sed "s/<REPO3>/conan-metadata/"| sed "s/<REPO4>/app-debian-sit-local/" | sed "s/<REPO5>/app-debian-uat-local/"  > permission.json
 curl -uadmin:${artifactory_pass} -XPUT http://${address}/artifactory/api/v2/security/permissions/${permission} -T permission.json -H "Content-Type: application/json"
 
 echo "------ Conan client configuration ------"
