e8fc52e979d95eceffa219275f1aaec41fc964dd
cyan21 <yann.chaysinh@gmail.com>
2020-04-27 16:31:13 +0200

    fixed lab14

--- labs/lab14.sh
+++ labs/lab14.sh
@@ -8,7 +8,6 @@ jfrog rt u debian_gen/myapp_1.0.deb app-debian-sit-local/pool/ --build-name=debi
 
 # define “dependency section” in build info
 jfrog rt bad debian-app 1 app_release.lock
-jfrog rt bad debian-app 1 conan_package.tgz
 
 # publish build info
 jfrog rt bp debian-app 1
