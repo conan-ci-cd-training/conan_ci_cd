dd83f1c571c99e9f4b7b622c0f7630b97ebb3e02
czoido <mrgalleta@gmail.com>
2020-05-21 18:35:17 +0200

    wip

--- setup_jenkins/init_jenkins.sh
+++ setup_jenkins/init_jenkins.sh
@@ -9,23 +9,45 @@ address="jfrog.local"
 artifactory_pass=$1
 jenkins_pass=$2
 
-echo "Clone libraries"
-
-cd /var/lib/jenkins
+echo "Config git"
 
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 
+echo "Configure conan"
+
 conan config install https://github.com/conan-ci-cd-training/settings.git
 
-git clone https://github.com/conan-ci-cd-training/libA.git
-git clone https://github.com/conan-ci-cd-training/libB.git
-git clone https://github.com/conan-ci-cd-training/libC.git
-git clone https://github.com/conan-ci-cd-training/libD.git
-git clone https://github.com/conan-ci-cd-training/App.git
-git clone https://github.com/conan-ci-cd-training/App2.git
-git clone https://github.com/conan-ci-cd-training/products.git
-git clone https://github.com/conan-ci-cd-training/release.git
+echo "Clone libraries"
+
+mkdir /git_server && cd /git_server
+
+git clone --bare https://github.com/conan-ci-cd-training/libA.git
+git clone --bare https://github.com/conan-ci-cd-training/libB.git
+git clone --bare https://github.com/conan-ci-cd-training/libC.git
+git clone --bare https://github.com/conan-ci-cd-training/libD.git
+git clone --bare https://github.com/conan-ci-cd-training/App.git
+git clone --bare https://github.com/conan-ci-cd-training/App2.git
+git clone --bare https://github.com/conan-ci-cd-training/products.git
+git clone --bare https://github.com/conan-ci-cd-training/release.git
+
+echo "curl http://${address}:8080/git/notifyCommit?url=/repos/libA.git" > /git_server/libA.git/hooks/post-receive
+echo "curl http://${address}:8080/git/notifyCommit?url=/repos/libB.git" > /git_server/libB.git/hooks/post-receive
+echo "curl http://${address}:8080/git/notifyCommit?url=/repos/libC.git" > /git_server/libC.git/hooks/post-receive
+echo "curl http://${address}:8080/git/notifyCommit?url=/repos/libD.git" > /git_server/libD.git/hooks/post-receive
+echo "curl http://${address}:8080/git/notifyCommit?url=/repos/App.git" > /git_server/App.git/hooks/post-receive
+echo "curl http://${address}:8080/git/notifyCommit?url=/repos/App2.git" > /git_server/App2.git/hooks/post-receive
+echo "curl http://${address}:8080/git/notifyCommit?url=/repos/products.git" > /git_server/products.git/hooks/post-receive
+echo "curl http://${address}:8080/git/notifyCommit?url=/repos/release.git" > /git_server/release.git/hooks/post-receive
+
+mkdir /bootstrap_repos && cd /bootstrap_repos
+
+git clone /git_server/libA.git
+git clone /git_server/libB.git
+git clone /git_server/libC.git
+git clone /git_server/libD.git
+git clone /git_server/App.git
+git clone /git_server/App2.git
 
 conan export libA mycompany/stable
 conan export libB mycompany/stable
@@ -44,11 +66,11 @@ conan upload '*' -r conan-develop --all --confirm  --force
 docker run --network="host" -it conanio/gcc6 /bin/bash -c "conan config install https://github.com/conan-ci-cd-training/settings.git;conan remote add conan-develop http://${address}:8081/artifactory/api/conan/conan-develop;conan user -p ${artifactory_pass} -r conan-develop admin;conan install App/1.0@mycompany/stable --profile debug-gcc6 --build missing -r conan-develop;conan install App2/1.0@mycompany/stable --profile debug-gcc6 --build missing -r conan-develop;conan upload '*' --all -r conan-develop --confirm  --force"
 docker run --network="host" -it conanio/gcc6 /bin/bash -c "conan config install https://github.com/conan-ci-cd-training/settings.git;conan remote add conan-develop http://${address}:8081/artifactory/api/conan/conan-develop;conan user -p ${artifactory_pass} -r conan-develop admin;conan install App/1.0@mycompany/stable --profile release-gcc6 --build missing -r conan-develop;conan install App2/1.0@mycompany/stable --profile release-gcc6 --build missing -r conan-develop;conan upload '*' --all -r conan-develop --confirm  --force"
 
-echo "------ Configure Jenkins jobs ------"
+#echo "------ Configure Jenkins jobs ------"
 
-curl https://raw.githubusercontent.com/conan-ci-cd-training/conan_ci_cd/master/setup_jenkins/jenkins_jobs.tgz -O
+#curl https://raw.githubusercontent.com/conan-ci-cd-training/conan_ci_cd/master/setup_jenkins/jenkins_jobs.tgz -O
 
-tar -xvf jenkins_jobs.tgz --directory /var/lib/jenkins/jobs
+#tar -xvf jenkins_jobs.tgz --directory /var/lib/jenkins/jobs
 
 echo "------ Restart Jenkins ------"
 
@@ -56,4 +78,4 @@ echo "--------------------------------"
 echo "------ FINISHED BOOTSTRAP ------"
 echo "--------------------------------"
 
-curl -X POST -u administrator:${jenkins_pass} http://${address}:8080/restart
+#curl -X POST -u administrator:${jenkins_pass} http://${address}:8080/restart
